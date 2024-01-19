fc4ff11045b0889080e6a0b31b856597202ca71c
a-mere-peasant <harshjain075@gmail.com>
2024-01-19 21:03:40 +0530

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import type {Component} from 'solid-js';
-import { For } from 'solid-js';
+import type {Component} from 'solid-js'
 import { TaskList } from './components/tasklist';
 import { useTaskContext } from './context/TaskContext';
-import { TaskCard } from './components/taskcard';
+import {Tabs, TabsList, TabsTrigger, TabsContent, TabsIndicator } from './components/ui/tabs';
+import { Task } from './components/task';
 
 const App: Component = () => {
 	const {tasks} = useTaskContext();
+	console.log(tasks.taskList.find(t=>t.tid==tasks.current));
 	return (
 			<>
-			<TaskList taskList ={tasks} />
+			<Tabs defaultValue="current">
+				<TabsContent value="tasklist">
+					<TaskList taskList ={tasks.taskList}/>
+				</TabsContent>
+				<TabsContent value = "current">
+					<Task task = {tasks.taskList.find(t=>t.tid==tasks.current)} />
+				</TabsContent>
+				<div class='absolute inset-x-0 bottom-0 flex bs-slate-900/75 bg-[#111]'>
+					<TabsTrigger value="tasklist" class='min-w-[50%] p-6'>Tasks</TabsTrigger>
+					<TabsTrigger value="current" class='min-w-[50%] p-6'>Current</TabsTrigger>
+				</div>
+			</Tabs>
 			</>
 	       );
 };
