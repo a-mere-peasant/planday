5f390d16eeb24108a0fc81860d91e2262b4a540b
a-mere-peasant <harshjain075@gmail.com>
2024-01-21 10:28:00 +0530

    feat: add new task navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Task } from './components/task';
 
 const App: Component = () => {
 	const {tasks} = useTaskContext();
-	console.log(tasks.taskList.find(t=>t.tid==tasks.current));
 	return (
 			<>
 			<Tabs defaultValue="current">
@@ -16,9 +15,13 @@ const App: Component = () => {
 				<TabsContent value = "current">
 					<Task task = {tasks.taskList.find(t=>t.tid==tasks.current)} />
 				</TabsContent>
+				<TabsContent value = "add">
+					<Task newTask={true} />
+				</TabsContent>
 				<div class='absolute inset-x-0 bottom-0 flex bs-slate-900/75 bg-[#111]'>
-					<TabsTrigger value="tasklist" class='min-w-[50%] p-6'>Tasks</TabsTrigger>
-					<TabsTrigger value="current" class='min-w-[50%] p-6'>Current</TabsTrigger>
+					<TabsTrigger value="tasklist" class='min-w-[33%] p-6'>Tasks</TabsTrigger>
+					<TabsTrigger value="current" class='min-w-[34%] p-6'>Current</TabsTrigger>
+					<TabsTrigger value="add" class='min-w-[33%] p-6'>New Task</TabsTrigger>
 				</div>
 			</Tabs>
 			</>
